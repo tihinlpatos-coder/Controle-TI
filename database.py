920bf24a679eab83045d12f1df2d1239d4d4912e
tihinlpatos-coder <tihinlpatos@gmail.com>
2026-06-30 10:26:52 -0300

    Implement SQL execution and query functions
    Added functions for executing and querying SQL commands.

--- database.py
+++ database.py
@@ -3,8 +3,56 @@ from config import Config
 
 
 def get_db():
-
     return psycopg2.connect(
         Config.DATABASE_URL,
         sslmode="require"
     )
+
+
+def executar(sql, parametros=None):
+    conn = get_db()
+    cur = conn.cursor()
+
+    if parametros:
+        cur.execute(sql, parametros)
+    else:
+        cur.execute(sql)
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+def consultar(sql, parametros=None):
+    conn = get_db()
+    cur = conn.cursor()
+
+    if parametros:
+        cur.execute(sql, parametros)
+    else:
+        cur.execute(sql)
+
+    dados = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return dados
+
+
+def consultar_um(sql, parametros=None):
+    conn = get_db()
+    cur = conn.cursor()
+
+    if parametros:
+        cur.execute(sql, parametros)
+    else:
+        cur.execute(sql)
+
+    dado = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return dado
